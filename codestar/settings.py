c899aaa8c3dd7444e5b5338ff4497a1485bc8fe5
wierdlygoodcoder <ashleyrolfe954@msn.com>
2022-10-11 08:59:53 +0000

    Likes

--- codestar/settings.py
+++ codestar/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -54,6 +55,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
